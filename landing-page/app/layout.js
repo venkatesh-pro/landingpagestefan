fa27a040cb8d17cfc303417eeb6db62a7b25bc9e
venkatesh k <venkateshk@venkateshs-MacBook-Pro.local>
2024-09-14 12:37:26 +0530

    added clarity

--- landing-page/app/layout.js
+++ landing-page/app/layout.js
@@ -2,6 +2,7 @@ import BackgroundPattern from "@/components/BackgroundPattern/BackgroundPattern"
 import "./globals.css";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import Script from "next/script"; // Import Next.js Script component
 
 export const metadata = {
   title: "Envoy",
@@ -38,10 +39,31 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script
+        {/* <script
           type="module"
           src="https://unpkg.com/@splinetool/viewer@1.9.21/build/spline-viewer.js"
-        ></script>
+        ></script> */}
+        {/* <script type="text/javascript">    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "o2wihkxxrk");
+</script> */}
+
+        <Script
+          src="https://unpkg.com/@splinetool/viewer@1.9.21/build/spline-viewer.js"
+          type="module"
+          strategy="afterInteractive" // Loads the script after page is interactive
+        />
+
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "o2wihkxxrk");`}
+        </Script>
+
         <link rel="icon" href="/logo.png" />
         <meta name="theme-color" content="#ffffff" />
       </head>
